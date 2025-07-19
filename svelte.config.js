ed2ab208736971262957696e564f3b50d87990cb
nathanroberts55 <nathan.roberts.15@cnu.edu>
2025-07-19 17:21:55 -0400

    I'm dumb

--- svelte.config.js
+++ svelte.config.js
@@ -6,12 +6,7 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: {
-		adapter: adapter(),
-		prerender: {
-			default: true
-		}
-	}
+	kit: { adapter: adapter() }
 };
 
 export default config;
